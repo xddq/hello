abeb7c18edd8bfff606700284cc46f001929133a
Pierre Dahmani <git@pierrefhapp.dev>
2022-10-27 22:31:25 +0200

    ran  but still got the error that the package only returns 1 value although the github package was updated to return 2... Perhaps it is a caching issue? Stop for now.

--- hello.go
+++ hello.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/xddq/greetings"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("Pierre")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	greeting, err := greetings.Hello("")
+
+	// Prints an error to the console and exits the program if an error was
+	// returned by the greeings.Hello function.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, we print the greeting to stdout.
+	fmt.Println(greeting)
 }
